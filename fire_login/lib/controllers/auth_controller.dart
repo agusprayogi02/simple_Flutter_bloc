a129b96453980e2fee63cfceb7f3013224edb439
agusprayogi02 <agus21apy@gmail.com>
2020-12-14 13:53:43 +0700

    (+) create auth controller

---
+++ fire_login/lib/controllers/auth_controller.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  Rx<User> _firebaseUser = Rx<User>();
+
+  User get user => _firebaseUser.value;
+
+  @override
+  void onInit() {
+    _firebaseUser.bindStream(_auth.authStateChanges());
+    super.onInit();
+  }
+
+  void signUp(String email, String pass) async {
+    try {
+      UserCredential _authResult = await _auth.createUserWithEmailAndPassword(
+        email: email.trim(),
+        password: pass,
+      );
+    } catch (e) {
+      Get.snackbar(
+        "Error creating Account",
+        e.message,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  void signIn(String email, String pass) async {
+    try {
+      UserCredential _authResult = await _auth.signInWithEmailAndPassword(
+        email: email.trim(),
+        password: pass,
+      );
+    } catch (e) {
+      Get.snackbar(
+        "Error Login Account",
+        e.message,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  void logOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {}
+  }
+}
